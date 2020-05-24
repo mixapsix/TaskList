603c9227ffc3df91d8560700e392255719daa92a
mixapsix <mixapsix1@mail.ru>
2020-05-24 12:17:59 +0300

    Добавил модели и контекст

---
+++ TaskList/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskList.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ TaskList/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskList.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
+    
